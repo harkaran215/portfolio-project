0d36c99a9ca8ba3f4a2c9d4fc5a703a58d19c636
harkaran215 <harkaran215@gmial.com>
2019-10-04 14:09:11 +0530

    added bootstrap and jobs are showing up

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
     
 
    
